d71e0937394cb216da78e4ac29db4fa4cab11499
AnjayGoel <anjaygoel@gmail.com>
2022-08-15 15:32:14 +0000

    Deploying to gh-pages from @ Pratham-Parmar/crm-frontend-new@7ac35e4bf5f158176a2f49d019010298fc1fe8e4 🚀

---
+++ precache-manifest.0ccd86e5c47a8b2910c0a63f7b423767.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "011942900af1657b9f8c",
+    "url": "/static/css/main.7a43e985.chunk.css"
+  },
+  {
+    "revision": "011942900af1657b9f8c",
+    "url": "/static/js/main.01194290.chunk.js"
+  },
+  {
+    "revision": "95ed461f5fd412e1400d",
+    "url": "/static/js/1.95ed461f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3b171665f9c58d2ece1c0f3251b6ae28",
+    "url": "/static/media/360_F_446228330_C8nqvb4hllsFg67RZfVQ32nOOGdtPmNv.3b171665.jpg"
+  },
+  {
+    "revision": "ec932268989ce8f99bd8f57b876ded57",
+    "url": "/index.html"
+  }
+];
